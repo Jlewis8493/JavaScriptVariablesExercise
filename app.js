d88c73422f09ad9419fd0c6515c70fc9f22c41c8
Jared Lewis <jlewis8493@gmail.com>
2022-02-12 14:21:59 -0500

    Exercise Complete

--- app.js
+++ app.js
@@ -4,7 +4,23 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Jared";
+let lastName = "Lewis";
+
+var myAge = 24.45;
+let isDone;
 
 // Exercise 2
+let fullName = firstName + " " + lastName;
+let templateFullName = '${firstName} ${lastName}';
+console.log("fullName: ", fullName);
+console.log("templateFullName: ", templateFullName);
 
 // Exercise 3
+let myStory;
+let city = "Boston";
+let pastime = "coding, running, reading, and hanging out with friends";
+
+myStory = "My name is " + fullName + ".\n " + "I live in " + city + ".\n " + "I like " + pastime + ".";
+myStory = 'My name is ${fullName}. I live in ${city}, I like to ${pastime}.';
+console.log(myStory); 
